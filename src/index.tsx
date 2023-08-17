e98ba4957ffc634758a41ad510f3d4beb1a44d5a
Marine <darbimar@mail.ru>
2023-08-17 17:17:52 +0700

    synchronize redux and localStorage with redux-persist

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 import { Provider } from 'react-redux';
-import { setupStore } from './store/store';
-
-const store = setupStore();
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistor, store } from './store/store';
+import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
 );
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,16 +1,40 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import todoReducer from './reducers/TodoSlice';
 
 const rootReducer = combineReducers({
   todoReducer,
 });
 
-export const setupStore = () => {
-  return configureStore({
-    reducer: rootReducer,
-  });
+const persistConfig = {
+  key: 'root',
+  storage,
 };
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
 export type RootState = ReturnType<typeof rootReducer>;
-export type AppStore = ReturnType<typeof setupStore>;
+export type AppStore = typeof store;
 export type AppDispatch = AppStore['dispatch'];
